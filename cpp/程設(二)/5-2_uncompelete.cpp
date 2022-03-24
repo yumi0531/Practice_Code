949ae93cc84a35b2eb38cdc5e1ce37a41cf7e4c9
yumi <yumi876122@gmail.com>
2022-03-25 00:16:11 +0800

    ok

--- cpp/程設(二)/5-2_uncompelete.cpp
+++ cpp/程設(二)/5-2_uncompelete.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+/* PRESET CODE BEGIN - NEVER TOUCH CODE BELOW */
 
+#include <bits/stdc++.h>
 using namespace std;
 
 struct mines
@@ -38,4 +39,30 @@ int main()
     }
     h = x_sort(c - 1);                //針對地雷的x軸做排序
     cout << detonate_count() << endl; //計算引爆次數
+}
+
+/* PRESET CODE END - NEVER TOUCH CODE ABOVE*/
+mines *x_sort(int k)
+{
+    // k = 所有地雷、減一
+    int x_arr[k + 1];
+    int i = 0;
+    mines *ptr; //指向此結構的指標
+    for (ptr = h; ptr->next != NULL; ptr = ptr->next)
+    {
+        x_arr[i] = ptr->x;
+        i++;
+    }
+
+    for (i = 0; i < k; i++)
+    {
+        if (x_arr[i] > x_arr[i + 1])
+        {
+            int temp = x_arr[i];
+            x_arr[i] = x_arr[i + 1];
+            x_arr[i + 1] = temp;
+        }
+    }
+
+    return x_arr[0];
 }
