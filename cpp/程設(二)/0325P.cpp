9bba6bed932e06795fab61784f951c8c66b3ffd5
yumi <yumi876122@gmail.com>
2022-03-26 23:16:09 +0800

    ok

--- cpp/程設(二)/0325P.cpp
+++ cpp/程設(二)/0325P.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
+/***加速***/
 void A_function(int speed[], int position[])
 {
     position[0] += speed[0];
     speed[0] *= 2;
 }
 
+/***重置速度***/
 void R_function(int speed[], int position[])
 {
     cout << "hi";
@@ -28,33 +30,46 @@ int main()
     {
         cin >> destination;
         int position[1] = {0}, speed[1] = {0};
+        bool state = true; // true -> 正向 , false -> 反向
         if (destination == 0)
         {
             break;
         }
+
         speed[0] = 1;
+
         while (true)
         {
+            state = true;
             if (position[0] < destination)
             {
                 A_function(speed, position);
+                state = true;
                 sum++;
-                // cout << "hi";
+            }
+            else if (position[0] > destination)
+            {
+                if (state == true)
+                {
+                    R_function(speed, position);
+                    state = false;
+                    sum++;
+                }
+                else
+                {
+                    A_function(speed, position);
+                    state = false;
+                    sum++;
+                }
             }
             else if (position[0] == destination)
             {
                 cout << sum << endl;
                 break;
             }
-            else if (position[0] > destination)
-            {
-                R_function(speed, position);
-                A_function(speed, position);
-                sum += 2;
-            }
         }
-        sum = 0;
     }
+    sum = 0;
 
     return 0;
 }
