d98295f4834417015b1fef7783c9b0e4acb0fd27
yumi <yumi876122@gmail.com>
2025-02-17 16:25:09 +0800

    新增 MyAlarmReceiver

---
+++ _Android_/Chapter8_Samples/AlarmClock/app/src/main/java/lincyu/alarmclock/MyAlarmReceiver.kt
@@ -0,0 +1,13 @@
+package lincyu.alarmclock
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class MyAlarmReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
+        TODO("MyAlarmReceiver.onReceive() is not implemented")
+    }
+}
